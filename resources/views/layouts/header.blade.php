8824f6ef2b279eb287762a2c251865b095f526d7
Ali Yousaf <54573081+aliyousaf1@users.noreply.github.com>
2022-10-26 20:46:10 +0500

    added registration and login form

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -35,7 +35,9 @@
                             <div @click="open = !open" class="relative py-3" :class="{'border-indigo-700 transform transition duration-300 ': open}" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100">
                                 <div class="flex justify-center items-center space-x-3 cursor-pointer">
                                     <div class="w-12 h-12 rounded-full overflow-hidden border-2 dark:border-white border-gray-900">
-                                        <img src="https://images.unsplash.com/photo-1610397095767-84a5b4736cbd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" alt="" class="w-full h-full object-cover">
+                                        <img src="{{auth()->user()->avatar}}" alt="">
+                                             alt=""
+                                             class="w-full h-full object-cover">
                                     </div>
                                 </div>
                                 <div x-show="open" style="left: -212px; top: 65px;" x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" class="absolute w-60 px-5 py-3 dark:bg-gray-800 bg-white rounded-lg shadow border dark:border-transparent mt-5">
