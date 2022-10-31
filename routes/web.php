a42703bcec6cda23b4b4af4c54bf83101250b834
mumar1052 <mc190402609@vu.edu.pk>
2022-10-31 16:09:30 +0500

    changes in pagesss

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,6 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\FrontendController;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\FrontendController;
-use App\Http\Controllers\DashboardController;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -36,13 +33,6 @@ Route::get('payment',[DashboardController::class,'approvePayment']);
 Route::get('description',[DashboardController::class,'description']);
 Route::get('marketing',[DashboardController::class,'marketingView']);
 Route::get('success',[DashboardController::class,'successPayment']);
-Route::get('SignUp', [FrontendController::class, 'register']);
-Route::get('SignIn', [FrontendController::class, 'login']);
-Route::get('forgetPassword', [FrontendController::class, 'forget']);
-Route::get('userProfile', [FrontendController::class, 'profile']);
-Route::get('home', [DashboardController::class, 'index'])->name('home');
-Route::get('add-contract', [DashboardController::class, 'viewContractForm']);
-Route::get('detail', [DashboardController::class, 'historyDetail']);
 
 Route::group(['prefix' => 'contract', 'middleware' => ['web','auth'], 'as' => 'contract.'], function () {
     Route::get('list', [ContractController::class, 'list'])->name('list');
