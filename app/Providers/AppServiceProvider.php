85a76e21d90f42c1cbcb8474dafca8923bc72d20
Ali Yousaf <54573081+aliyousaf1@users.noreply.github.com>
2022-10-31 18:39:47 +0500

    added build

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\Contract;
+use App\Models\ContractStatus;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,6 +27,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Contract::query()->delete();
+        ContractStatus::query()->delete();
+        DB::table('contract_user')->delete();
         Model::preventLazyLoading(!app()->isProduction());
 //        Model::preventSilentlyDiscardingAttributes(!app()->isProduction());
         Model::unguard();
