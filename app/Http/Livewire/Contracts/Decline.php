df16048758064cd39fa4cb6614d6b7f1f0edf8b8
mumar1052 <mc190402609@vu.edu.pk>
2022-11-02 12:10:20 +0500

    changes in decine modal

--- app/Http/Livewire/Contracts/Decline.php
+++ app/Http/Livewire/Contracts/Decline.php
@@ -2,13 +2,23 @@
 
 namespace App\Http\Livewire\Contracts;
 
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Contracts\HasForms;
 use LivewireUI\Modal\ModalComponent;
 
-class Decline extends ModalComponent
+class Decline extends ModalComponent implements HasForms
 {
+    use InteractsWithForms;
     public function render()
     {
         return view('livewire.contracts.decline');
+    }
+    protected function getFormSchema(): array
+    {
+        return [
+            Textarea::make('reason')->disableLabel()
+        ];
     }
 
     public function submit()
--- resources/views/livewire/contracts/decline.blade.php
+++ resources/views/livewire/contracts/decline.blade.php
@@ -4,18 +4,20 @@
         {{--                <img class="relative object-cover w-full h-full rounded-xl" src="{{asset('assets/images/card.png')}}">--}}
 
         <div class="w-full px-8 absolute top-8">
-            <h3 class="text-center text-black mb-4 ml-0 font-bold text-2xl">Reason</h3>
-            <div class="flex justify-center">
-                <div>
-                    <textarea class="text-black" name="description" id="" cols="30" rows="7"></textarea>
+            <h3 class="text-center text-black mb-4 ml-0 font-bold text-2xl">
+                Reason
+            </h3>
+                <div class="max-w-full md:w-full sm:w-full p-6 text-black">
+{{--                    <textarea class="text-black" name="description" id="" cols="30" rows="7"></textarea>--}}
+                    {{$this->form}}
                 </div>
-            </div>
-            <div class="pt-1 flex justify-around">
-                <button wire:click="$emit('closeModal')" class="bg-green-500 hover:bg-green-700  text-white font-bold py-2 px-8 rounded-sm text-xl mt-14">
-                    Ok
+            <div class=" gap-6 flex flex-row justify-center rounded-md shadow-sm sm:col-span-2 mt-10" role="group">
+                <button type="button" onclick="Livewire.emit('')"
+                        class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
+                    <span class="ml-2">OK </span>
                 </button>
-                <button  wire:click="$emit('closeModal')" class="bg-red-500 hover:bg-red-700  text-white font-bold py-2 px-4 rounded-sm text-xl mt-14">
-                    Cancel
+                <button type="button"  onclick="Livewire.emit('')"  class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
+                    <span class="ml-2">Cancel</span>
                 </button>
             </div>
         </div>
