860042e4ccbab34fea809f58dce50bab6c82256a
Rizwan Nasir <rizwannasir117@gmail.com>
2022-12-16 16:58:16 +0500

    updated deleting product route changed to delete

---
+++ app/Filament/Resources/ContractResource/Pages/ViewContract.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\ContractResource\Pages;
+
+use App\Filament\Resources\ContractResource;
+use Filament\Pages\Actions;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewContract extends ViewRecord
+{
+    protected static string $resource = ContractResource::class;
+}
