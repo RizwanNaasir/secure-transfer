f7a31b12556477bc348bf543e94c28f18dc58d61
Rizwan Nasir <rizwannasir117@gmail.com>
2023-01-05 15:17:11 +0500

    removed edit/delete actions from product card

--- resources/views/components/product/card.blade.php
+++ resources/views/components/product/card.blade.php
@@ -19,32 +19,32 @@
                 </div>
             </div>
         </a>
-        @if($product->authUserIsOwner())
-            <form id="delete-{{$product->id}}" action="{{route('user.product.delete',$product->id)}}" method="POST">
-                @csrf
-                @method('DELETE')
-            </form>
+{{--        @if($product->authUserIsOwner())--}}
+{{--            <form id="delete-{{$product->id}}" action="{{route('user.product.delete',$product->id)}}" method="POST">--}}
+{{--                @csrf--}}
+{{--                @method('DELETE')--}}
+{{--            </form>--}}
 
-            <a href="#" onclick="if (confirm('{{ __('lang.confirmation') }}')) {
-                                           event.preventDefault();
-                                           document.getElementById('delete-{{$product->id}}').submit();
-                                         }else{
-                                           event.preventDefault();
-                                         }">
-                <button class="absolute top-0 bg-transparent text-white p-2 right-1 flex  rounded hover:bg-black-800 py-0 px-0">
-                    <span><svg class="w-6 h-6 invisible group-hover/item:visible fill-gray-800 stroke-black" fill="none"
-                               stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path
-                                    stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                    d="M6 18L18 6M6 6l12 12"></path></svg></span></button>
-            </a>
-            {{--                                    <button onclick="window.location.href='{{route('user.product.delete',$product->id)}}'" class="absolute top-0 bg-transparent text-white p-2 right-1 flex  rounded hover:bg-black-800 py-0 px-0"><span><svg class="w-6 h-6 invisible group-hover/item:visible fill-gray-800 stroke-black" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg></span></button>--}}
-            <button onclick="window.location.href='{{route('user.product.edit',$product->id)}}'"
-                    class="absolute bottom-20 bg-transparent text-white p-2 right-1 flex  rounded hover:bg-black-800 py-0 px-0">
-                <span><svg class="w-8 h-8 invisible group-hover/item:visible  fill-gray-600" fill="none"
-                           stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path
-                                stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path></svg></span>
-            </button>
-        @endif
+{{--            <a href="#" onclick="if (confirm('{{ __('lang.confirmation') }}')) {--}}
+{{--                                           event.preventDefault();--}}
+{{--                                           document.getElementById('delete-{{$product->id}}').submit();--}}
+{{--                                         }else{--}}
+{{--                                           event.preventDefault();--}}
+{{--                                         }">--}}
+{{--                <button class="absolute top-0 bg-transparent text-white p-2 right-1 flex  rounded hover:bg-black-800 py-0 px-0">--}}
+{{--                    <span><svg class="w-6 h-6 invisible group-hover/item:visible fill-gray-800 stroke-black" fill="none"--}}
+{{--                               stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path--}}
+{{--                                    stroke-linecap="round" stroke-linejoin="round" stroke-width="2"--}}
+{{--                                    d="M6 18L18 6M6 6l12 12"></path></svg></span></button>--}}
+{{--            </a>--}}
+{{--            --}}{{--                                    <button onclick="window.location.href='{{route('user.product.delete',$product->id)}}'" class="absolute top-0 bg-transparent text-white p-2 right-1 flex  rounded hover:bg-black-800 py-0 px-0"><span><svg class="w-6 h-6 invisible group-hover/item:visible fill-gray-800 stroke-black" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg></span></button>--}}
+{{--            <button onclick="window.location.href='{{route('user.product.edit',$product->id)}}'"--}}
+{{--                    class="absolute bottom-20 bg-transparent text-white p-2 right-1 flex  rounded hover:bg-black-800 py-0 px-0">--}}
+{{--                <span><svg class="w-8 h-8 invisible group-hover/item:visible  fill-gray-600" fill="none"--}}
+{{--                           stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path--}}
+{{--                                stroke-linecap="round" stroke-linejoin="round" stroke-width="2"--}}
+{{--                                d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path></svg></span>--}}
+{{--            </button>--}}
+{{--        @endif--}}
     </div>
 </li>
