748963a9d0b54fdc807f56b58d8eb7f30227d742
Rizwan Nasir <rizwannasir117@gmail.com>
2023-01-17 15:15:29 +0500

    updated mail configurations so mail can be sent now.

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -16,7 +16,7 @@
         <div>
             <x-input-label for="email" :value="__('Email')"/>
 
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email"
+            <x-text-input readonly id="email" class="block mt-1 w-full" type="email" name="email"
                           :value="old('email', $request->email)" required autofocus/>
 
             <x-input-error :messages="$errors->get('email')" class="mt-2"/>
