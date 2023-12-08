7106870e2921938bfbf811e7485b9ded4c3194df
bergforever <mumar1052@gmail.com>
2023-12-08 15:27:59 +0500

    Update register form style on homepage
    The styles on the register form located on the homepage have been modified for improved usability. Changes include centering the registration header and adjusting the positioning of the registration button to better fit the page's overall design.

--- resources/views/livewire/auth/register-from-home.blade.php
+++ resources/views/livewire/auth/register-from-home.blade.php
@@ -1,10 +1,10 @@
 <div class="py-10 px-6 sm:px-10 xl:p-12 col-span-2 sm:col-span-2">
-    <h3 class="text-lg font-medium text-warm-gray-900">
+    <h3 class="text-lg ml-56 font-semibold text-center font-medium text-warm-gray-900">
         {{__('lang.register')}}
     </h3>
     <form wire:submit.prevent="submit">
         {{ $this->form }}
-        <x-reactive-button class="w-full">
+        <x-reactive-button style="margin-left: 35%" class="flex justify-end mx-auto justify-content-end" class="w-2/3">
             {{ __('lang.register') }}
         </x-reactive-button>
     </form>
