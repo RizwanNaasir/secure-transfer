998e1552d843916fc50819ba9a3883d5f248a2da
bergforever <mumar1052@gmail.com>
2023-12-08 16:38:53 +0500

    Update CSS file reference and adjust form styles
    The commit reflects the updated CSS file in the application manifest.json and modifies the layout of the registration form in the 'register-from-home' view. Specifically, it alters the styles for a headline and a button, to improve visual alignment and usability.

--- resources/views/livewire/auth/register-from-home.blade.php
+++ resources/views/livewire/auth/register-from-home.blade.php
@@ -1,10 +1,10 @@
 <div class="py-10 px-6 sm:px-10 xl:p-12 col-span-2 sm:col-span-2">
-    <h3 class="text-lg ml-56 font-semibold text-center font-medium text-warm-gray-900">
+    <h3 style="margin-left: 38%" class="text-lg flex justify-center  content-center font-semibold text-center text-warm-gray-900">
         {{__('lang.register')}}
     </h3>
     <form wire:submit.prevent="submit">
         {{ $this->form }}
-        <x-reactive-button style="margin-left: 35%" class="flex justify-end mx-auto justify-content-end" class="w-2/3">
+        <x-reactive-button class="w-[62%] text-center float-right ">
             {{ __('lang.register') }}
         </x-reactive-button>
     </form>
